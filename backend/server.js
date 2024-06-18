c000c6fd3213484dac03f01a0992c5d821804d54
Nargis Fatime <nargis.fatima160@gmail.com>
2024-06-18 17:47:33 +0530

    all okay

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 import products from './data/products.js';
-const port=4000;
+
+const port= process.env.PORT || 4000
 
 const app= express();
 
 app.get('/', (req, res) => {
-    res.send('API is running ...')
+    res.send('API is running fast ...')
 })
 
 app.get('/api/products', (req, res) => {
     res.json(products);
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
 })
 
 app.listen(port, () => console.log(`server running on port ${port}`));
