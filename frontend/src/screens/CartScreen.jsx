713f9adac185c5b21da6ab98e3e20d09e823f559
Nargis Fatime <nargis.fatima160@gmail.com>
2024-06-29 17:55:15 +0530

    checkout done

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -19,6 +19,10 @@ const CartScreen = () => {
     const removeFromCartHandler = async(id) => {
         dispatch(removeFromCart(id));
     }
+
+    const checkOutHandler = () => {
+        navigate('/login?redirect=/shipping')
+    }
     
   return (
     <Row>
@@ -78,7 +82,9 @@ const CartScreen = () => {
                                     ${ cartItems.reduce(( acc, item) => acc + item.qty * item.price, 0).toFixed(2) }
                                 </ListGroup.Item>
                                 <ListGroup.Item>
-                                    <Button type="button" className="btn-block" disabled={ cartItems.length === 0} >
+                                    <Button type="button" className="btn-block" disabled={ cartItems.length === 0} 
+                                    onClick={checkOutHandler}
+                                    >
                                         Proceed to Checkout 
                                     </Button>
                                 </ListGroup.Item>
