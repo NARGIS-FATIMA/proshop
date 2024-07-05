c37fef5903416e5b659b4fb82a504e6e0eb278e6
Nargis Fatime <nargis.fatima160@gmail.com>
2024-07-05 10:54:47 +0530

    Private routes added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,14 @@ import store from './store';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
+import PrivateRoute from './components/PrivateRoute';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const router=createBrowserRouter(
   createRoutesFromElements(
@@ -21,6 +23,11 @@ const router=createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+      <Route path="/shipping" element={<ShippingScreen />} />
+
+      <Route path='' element={<PrivateRoute />} >
+      <Route path='/shipping' element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 )
