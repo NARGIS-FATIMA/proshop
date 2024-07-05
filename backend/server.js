95a9e1898da68f56a5f6696ff98e811d986ea593
Nargis Fatime <nargis.fatima160@gmail.com>
2024-07-05 21:21:50 +0530

    order routes and controller

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,12 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 const port = process.env.PORT || 5000
 
 connectDB();
+
 
 const app= express();
 
@@ -27,6 +29,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
