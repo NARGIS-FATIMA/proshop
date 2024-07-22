e75d9e906734365a28469254f5e06f20bf99aff7
Nargis Fatime <nargis.fatima160@gmail.com>
2024-07-22 11:02:57 +0530

    Search Box added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,6 +5,7 @@ import {LinkContainer} from 'react-router-bootstrap';
 import logo from '../assets/logo.png';
 import { useSelector, useDispatch } from 'react-redux';
 import { useLogoutMutation } from '../slices/usersApiSlice';
+import SearchBox from './SearchBox';
 import { logout } from '../slices/authSlice';
 
 
@@ -41,6 +42,7 @@ const Header = () => {
                 <Navbar.Toggle aria-controls='basic-navbar-nav' />
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='ms-auto'>
+                        <SearchBox />
                         <LinkContainer to="cart">
                         <Nav.Link>
                             <FaShoppingCart />Cart
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -5,16 +5,17 @@ import Loader from "../components/Loader";
 import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Message from "../components/Message";
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Paginate from '../components/Paginate';
 
 const HomeScreen = () => {
   
-  const { pageNumber } = useParams();
-  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+  const { pageNumber, keyword } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ keyword, pageNumber });
   
   return (
     <>
+        { keyword && <Link to='/' className='btn btn-light mb-4'>Go Back</Link> }
         {isLoading ? (
           <Loader />
         ) : error ? (<Message variant='danger'>{ error?.data?.message || error.error }</Message>) : ( <>
@@ -29,6 +30,7 @@ const HomeScreen = () => {
           <Paginate
             pages = {data.pages}
             page = {data.page}
+            keyword = {keyword ? keyword : ''}
           />
           
         </>) }
